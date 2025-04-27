d870c5c5974e8c4ec75fad61c8ad014e473e19cf
emillydotcom <125111411524@ulife.com.br>
2025-04-26 22:02:38 -0300

    Multiplicacao5.java
    Exercício 1: Tabela de Multiplicação do Número 5
    Enunciado:
    Crie um programa que imprima a tabela de multiplicação do número 5, de 1 até 10, utilizando um loop for.

---
+++ Loop_For/Multiplicacao5.java
@@ -0,0 +1,12 @@
+// Atividade 1
+public class Multiplicacao5 {
+    public static void main(String[] args) {
+
+        // Loop de 1 até 10
+        for (int i = 1; i <= 10; i++) {
+            int resultado = 5 * i; 
+            // Multiplicando 5 pelo número atual (i)
+            System.out.println("5 x " + i + " = " + resultado); 
+        }
+    }
+}
