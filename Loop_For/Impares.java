58549088a4d2e827bcf30a11676cfced9610e0f4
emillydotcom <125111411524@ulife.com.br>
2025-04-26 22:17:34 -0300

    Impares.java
    Exercício 4: Impressão de Números Ímpares
    Enunciado:
    Desenvolva um programa que utilize um loop for para imprimir todos os números ímpares entre 1 e 100.

---
+++ Loop_For/Impares.java
@@ -0,0 +1,11 @@
+// Atividade 4 
+public class Impares {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + ","); 
+            }
+        }
+    }
+}
